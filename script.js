c1be01e964ce925e84024adf9906962fe74d446c
AshishGangera <164016949+AshishGangera@users.noreply.github.com>
2024-04-13 16:13:49 +0530

    Add files via upload
    day2

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeBackground_color() {
+    var color = document.getElementById("color_name").value;
+    document.bgColor = color;
+}
+
+document.getElementById("submitColor").addEventListener;
